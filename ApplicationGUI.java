e9f3f4375e28f2ca77a2840a52cc4da236901dbc
Mohamad W. Alhalabi <mh19.my@gmail.com>
2017-12-11 20:22:49 +0200

    Update ApplicationGUI.java

--- ApplicationGUI.java
+++ ApplicationGUI.java
@@ -1,6 +1,7 @@
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
@@ -13,6 +14,7 @@ public class ApplicationGUI {
     private JComboBox comboBox1;
     private JButton generateButton;
     private JLabel CharactersLabel;
+    private JLabel CharactersKeyLabel;
 
     private String generateKey(int max) throws NoSuchAlgorithmException {
         KeyGenerator gen = KeyGenerator.getInstance("AES");
@@ -24,15 +26,70 @@ public class ApplicationGUI {
     private String toHex(String arg) {
         return String.format("%040x", new BigInteger(1, arg.getBytes(/*YOUR_CHARSET?*/)));
     }
+    private String separate(String s){
+        s= s.replaceAll("..", "$0 ");
+        StringBuilder sb = new StringBuilder(s);
+        sb.replace(11,12,"\n");
+        sb.replace(23,24,"\n");
+        return sb.replace(35,36,"\n").toString();
+    }
+
 
     private ApplicationGUI()
     {
 
-
         encryptButton.addActionListener(e -> {
             if (textField1.getText().length()==16 && textField2.getText().length()!=0) {
-                AES aes = new AES(toHex(textField1.getText()), textField2.getText());
-                aes.encrypt(toHex(textField1.getText()), textField2.getText());
+                int max=128;
+                if (comboBox1.getSelectedItem().toString().equals("192-bit"))
+                    max=192;
+                else if(comboBox1.getSelectedItem().toString().equals("256-bit"))
+                {
+                    max=256;
+                }
+                if(max==128 && textField2.getText().length()==32) {
+                    if(textField2.getText().contains("A") || textField2.getText().contains("B") || textField2.getText().contains("C") || textField2.getText().contains("D") || textField2.getText().contains("E") || textField2.getText().contains("F")) {
+                        AES aes = new AES(toHex(textField1.getText()), textField2.getText(), max);
+                        JOptionPane.showMessageDialog(null, separate(aes.encrypt(toHex(textField1.getText()), textField2.getText())));
+                    }
+                    else JOptionPane.showMessageDialog(null, "The key has an invalid hex character.");
+                /*JFrame f = new JFrame("JTable Sample");
+                f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                Container content = f.getContentPane();
+                Object rows[][] = { { aes.encrypt(toHex(textField1.getText()), textField2.getText()).substring(0,2), "Amazon", "67 9/16" },
+                        { "AOL", "America Online", "68 3/4" },
+                        { "BOUT", "About.com", "56 3/8" },
+                        { "CDNW", "CDnow", "4 7/16" },
+                        { "DCLK", "DoubleClick", "87 3/16" },
+                        { "EBAY", "eBay", "180 7/8" },
+                        { "EWBX", "EarthWeb", "18 1/4" },
+                        { "MKTW", "MarketWatch", "29" },
+                        { "TGLO", "Theglobe.com", "4 15/16" },
+                        { "YHOO", "Yahoo!", "151 1/8" } };
+                Object columns[] = { "Symbol", "Name", "Price" };
+                JTable table = new JTable(rows, columns);
+                JScrollPane scrollPane = new JScrollPane(table);
+                content.add(scrollPane, BorderLayout.CENTER);
+                f.setSize(300, 200);
+                f.setVisible(true);*/
+                }
+                else if(max==192 && textField2.getText().length()==48)
+                {
+                    if(textField2.getText().contains("A") || textField2.getText().contains("B") || textField2.getText().contains("C") || textField2.getText().contains("D") || textField2.getText().contains("E") || textField2.getText().contains("F")) {
+                        AES aes = new AES(toHex(textField1.getText()), textField2.getText(), max);
+                        JOptionPane.showMessageDialog(null, separate(aes.encrypt(toHex(textField1.getText()), textField2.getText())));
+                    }
+                    else JOptionPane.showMessageDialog(null, "The key has an invalid hex character.");
+                }
+                else if(max==256 && textField2.getText().length()==64)
+                {
+                    if(textField2.getText().contains("A") || textField2.getText().contains("B") || textField2.getText().contains("C") || textField2.getText().contains("D") || textField2.getText().contains("E") || textField2.getText().contains("F")) {
+                        AES aes = new AES(toHex(textField1.getText()), textField2.getText(), max);
+                        JOptionPane.showMessageDialog(null, separate(aes.encrypt(toHex(textField1.getText()), textField2.getText())));
+                    }
+                    else JOptionPane.showMessageDialog(null, "The key has an invalid hex character.");
+                }
+                else JOptionPane.showMessageDialog(null, "Key has wrong size.");
             }
             else if(textField2.getText().length()==0)
                 JOptionPane.showMessageDialog(null, "No key was entered");
@@ -55,9 +112,48 @@ public class ApplicationGUI {
 
         textField1.addKeyListener(new KeyAdapter() {
             @Override
-            public void keyTyped(KeyEvent e) {
-                super.keyTyped(e);
-                CharactersLabel.setText("Characters Remaining: " + (15 - textField1.getText().length()));
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+                CharactersLabel.setText("Characters Remaining: " + (16 - textField1.getText().length()));
+            }
+        });
+        comboBox1.addActionListener(e -> {
+            CharactersKeyLabel.setText("Characters Remaining: " + (32 - textField2.getText().length()));
+            if(comboBox1.getSelectedItem().toString().equals("192-bit"))
+                CharactersKeyLabel.setText("Characters Remaining: " + (48 - textField2.getText().length()));
+            else if(comboBox1.getSelectedItem().toString().equals("256-bit"))
+                CharactersKeyLabel.setText("Characters Remaining: " + (64 - textField2.getText().length()));
+        });
+        textField2.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+                if(comboBox1.getSelectedItem().toString().equals("128-bit"))
+                    CharactersKeyLabel.setText("Characters Remaining: " + (32 - textField2.getText().length()));
+                else if(comboBox1.getSelectedItem().toString().equals("192-bit"))
+                    CharactersKeyLabel.setText("Characters Remaining: " + (48 - textField2.getText().length()));
+                else if(comboBox1.getSelectedItem().toString().equals("256-bit"))
+                    CharactersKeyLabel.setText("Characters Remaining: " + (64 - textField2.getText().length()));
+            }
+        });
+        textField2.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                if(comboBox1.getSelectedItem().toString().equals("128-bit"))
+                    CharactersKeyLabel.setText("Characters Remaining: " + (32 - textField2.getText().length()));
+                else if(comboBox1.getSelectedItem().toString().equals("192-bit"))
+                    CharactersKeyLabel.setText("Characters Remaining: " + (48 - textField2.getText().length()));
+                else if(comboBox1.getSelectedItem().toString().equals("256-bit"))
+                    CharactersKeyLabel.setText("Characters Remaining: " + (64 - textField2.getText().length()));
+
+            }
+        });
+        textField1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                CharactersLabel.setText("Characters Remaining: " + (16 - textField1.getText().length()));
             }
         });
     }
